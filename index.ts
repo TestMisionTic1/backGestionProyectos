3f1e84276568f0d4e2850c722c4376abf6ea7c6a
David Puerto <dsp5502@gmail.com>
2021-11-12 17:43:04 -0500

    agregar relaciones fuertes

--- index.ts
+++ index.ts
@@ -1,25 +1,57 @@
 import conectarDb from "./db/db";
 import { userModel } from "./models/user";
-import { Enum_Rol } from "./models/enums";
+import { Enum_Rol, Enum_TipoObjetivo } from "./models/enums";
+import { projectModel } from "./models/project";
+import { ObjectiveModel } from "./models/objective";
 
 
 const main = async () => {
   await conectarDb();
 
-  // crear un usuario
+  // const object =  await ObjectiveModel.create({
+  //   descripcion: " Este es el Objetivo Especifico",
+  //   tipo: Enum_TipoObjetivo.especifico
+  // })
+
+//Crear Proyecto
+  // await projectModel.create({
+  // nombre: "Mintic",
+  // presupuesto: 10.000,
+  // fechaInicio: Date.now(),
+  // fechaFin: new Date("2021/11/10"),
+  // lider: "618eb4cb8ae752bc9d191b80",
+  // objetivos:[ "618eeb00f9695afa5a6443de", "618eeb7222e8cf5ede833025"]
+  // })
+
+//consultar proyecto
+const proyecto= await projectModel.find({nombre: "Mintic"}).populate("lider").populate("objetivos")
+console.log("El proyecto es:  ", JSON.stringify(proyecto))
+
+};
+  main();
+
+
+
+
+
+
+
+  //CRUD USUARIO
+
+   // crear un usuario
   
- await userModel.create({
-    nombre: "David",
-    apellido: "Salguero",
-    correo: "dsp5502@hotmail.com",
-    identificacion: "45464",
-    rol: Enum_Rol.lider
-  }).then((u) => {
-      console.log("usuario Creado", u);
-    })
-    .catch((e) => {
-      console.error("Error creando el usuario", e);
-    });
+//  await userModel.create({
+//     nombre: "David",
+//     apellido: "Salguero",
+//     correo: "dsp5502@gmail.com",
+//     identificacion: "10324447",
+//     rol: Enum_Rol.lider
+//   }).then((u) => {
+//       console.log("usuario Creado", u);
+//     })
+//     .catch((e) => {
+//       console.error("Error creando el usuario", e);
+//     });
 
 
 // obtener los usuarios 
@@ -30,5 +62,39 @@ const main = async () => {
 //       console.error("Error obteniendo los usuario", e);
 //     });
 
-};
-  main();
+
+//OBTENER UN SOLO USUARIO
+
+// await userModel.findOne({identificacion: "45464"}).then((u) => {
+//       console.log("usuario Encontrado", u);
+//     })
+//     .catch((e) => {
+//       console.error( e);
+//     });
+
+
+//EDITAR UN USUARIO:
+
+// await userModel.findOneAndUpdate(
+//   {correo: "dsp5502@gmail.com" },
+//   {
+//     nombre: "Daniel",
+//     apellido:"Lopez",
+//   }).then((u) => {
+//       console.log("usuario Editado", u);
+//     })
+//     .catch((e) => {
+//       console.error("Error Editando el usuario", e);
+//     });
+
+
+//eliminar Ususario
+// await userModel.findOneAndDelete(
+//   {correo: "dsp5502@gmail.com"},
+
+//   ).then((u) => {
+//       console.log("usuario Eliminado", u);
+//     })
+//     .catch((e) => {
+//       console.error("Error Eliminado usuario", e);
+//     });
--- models/enums.ts
+++ models/enums.ts
@@ -4,5 +4,27 @@ enum Enum_Rol{
   administrador = "Administrador"
   
 }
+enum Enum_EstadoUsuario{
+ Pendiente = "Pendiente",
+  autorizado = "Autorizado",
+  noAutorizado = "No Autorizado"
+  
+}
+enum Enum_EstadoProyecto{
+  activo= "Activo",
+  inactivo= "Inactivo",
 
-export {Enum_Rol}
+}
+
+enum Enum_FaseProyecto{
+  iniciado= "Iniciado",
+  desarrollo= "Desarrollo",
+  terminado= "Termiando",
+  nula = " ",
+}
+enum Enum_TipoObjetivo{
+  general = "General",
+  especifico = "Especifico"
+}
+
+export {Enum_Rol, Enum_EstadoUsuario, Enum_EstadoProyecto, Enum_FaseProyecto, Enum_TipoObjetivo}
